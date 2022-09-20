235e07f85646bef6c43c0e11acc3cd6b9498dbaf
PopotehPanda <ptikhomi@iu.edu>
2022-09-20 11:08:56 -0400

    included w3 styling template

--- src/App.js
+++ src/App.js
@@ -10,25 +10,63 @@ import CreateBook from './CreateBook';
 import EditBook from './EditBook';
 import FilterBooks from './FilterBooks';
 import UpdateProfile from './UpdateProfile';
+import { w3_close, w3_open } from './utils/w3utils';
 
 function App() {
+
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<FilterBooks />} />
-          <Route path="/book/:bookId/chapter/:chapterId" element={<Book />} />
-          <Route path="/write/:bookId/chapter/:chapterId" element={<WriteBook />} />
-          <Route path="/edit/:bookId" element={<EditBook />} />
-          <Route path="/dashboard" element={<ListWrittenBooks />} />
-          <Route path="/create-book" element={<CreateBook />} />
-          <Route path="/update-profile" element={<UpdateProfile/>} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      {/* Sidebar menu */}
+      <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style={{ zIndex: '3', width: '300px' }} id="mySidebar"><br />
+        <div class="w3-container">
+          <a href="#" onClick={w3_close} class="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey" title="close menu">
+            <i class="fa fa-remove"></i>
+          </a>
+          <img src="/w3images/avatar_g2.jpg" style={{ width: '45%' }} class="w3-round" /><br /><br />
+          <a href="/"><h4><b>BOOK CLUB</b></h4></a>
+        </div>
+        <div class="w3-bar-block">
+          <a href="#portfolio" onClick={w3_close} class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>PORTFOLIO</a>
+          <a href="#about" onClick={w3_close} class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>ABOUT</a>
+          <a href="#contact" onClick={w3_close} class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>CONTACT</a>
+        </div>
+      </nav>
+
+      {/* Overlay effect for small screens */}
+      <div class="w3-overlay w3-hide-large w3-animate-opacity" onClick={w3_close} style={{ cursor: 'pointer' }} title="close side menu" id="myOverlay"></div>
+
+      {/* Page content */}
+      <div class="w3-main" style={{ marginLeft: '300px' }}>
+        { /* Header */}
+        <header>
+          <a href="#"><img src="/w3images/avatar_g2.jpg" style={{ width: '65px' }} class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity" /></a>
+          <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onClick={w3_open} ><i class="fa fa-bars"></i></span>
+          <div class="w3-container">
+            <div></div>
+            <h1><b>Book Club</b></h1>
+            <div class="w3-section w3-bottombar"></div>
+          </div>
+        </header>
+
+        <div>
+          <BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<FilterBooks />} />
+              <Route path="/book/:bookId/chapter/:chapterId" element={<Book />} />
+              <Route path="/write/:bookId/chapter/:chapterId" element={<WriteBook />} />
+              <Route path="/edit/:bookId" element={<EditBook />} />
+              <Route path="/dashboard" element={<ListWrittenBooks />} />
+              <Route path="/create-book" element={<CreateBook />} />
+              <Route path="/update-profile" element={<UpdateProfile />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
+    </>
+
   );
 }
 
--- src/FilterBooks.js
+++ src/FilterBooks.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { domainAPI } from './utils/mongoDBConnect';
+import { w3_close, w3_open } from './utils/w3utils';
 
 function FilterBooks() {
   const [currPage, setCurrPage] = useState(0);
@@ -18,31 +19,31 @@ function FilterBooks() {
 
     // Get the total number of books (for computing pages)
     // AND the current page's book list information
-    const result = await axios.get(domainAPI + "book/filter/" + genre + "/" + language + "/" + views + "/" + wordcount + "/" +  numberPerPage + "/" + currPage, {crossdomain: true})
-        .then((result) => {
-          return result.data;
-        })
-        .catch((error) => {
-          console.log("error");
-          console.log(error);
-        });
-      setListBooksOnPage([...result.books]);
-      setTotalNumOfPages(Math.ceil(result.documentCount / numberPerPage));
+    const result = await axios.get(domainAPI + "book/filter/" + genre + "/" + language + "/" + views + "/" + wordcount + "/" + numberPerPage + "/" + currPage, { crossdomain: true })
+      .then((result) => {
+        return result.data;
+      })
+      .catch((error) => {
+        console.log("error");
+        console.log(error);
+      });
+    setListBooksOnPage([...result.books]);
+    setTotalNumOfPages(Math.ceil(result.documentCount / numberPerPage));
   }
   function firstPage() {
     setCurrPage(0);
     window.scrollTo(0, 0);
   }
   function prevPage() {
-    if(currPage > 0) setCurrPage(currPage-1);
+    if (currPage > 0) setCurrPage(currPage - 1);
     window.scrollTo(0, 0);
   }
   function nextPage() {
-    if(currPage < totalNumOfPages-1) setCurrPage(currPage+1);
+    if (currPage < totalNumOfPages - 1) setCurrPage(currPage + 1);
     window.scrollTo(0, 0);
   }
   function lastPage() {
-    setCurrPage(totalNumOfPages-1);
+    setCurrPage(totalNumOfPages - 1);
     window.scrollTo(0, 0);
   }
 
@@ -60,87 +61,123 @@ function FilterBooks() {
 
   return (
     <>
+      {/* Filtering books */}
+      <header id="portfolio">
+        <div class="w3-container">
+          <div class="w3-section w3-padding-16">
+            <span class="w3-margin-right">Filter:</span>
+            <button class="w3-button w3-black">ALL</button>
+            <button class="w3-button w3-white"><i class="fa fa-diamond w3-margin-right"></i>Design</button>
+            <button class="w3-button w3-white w3-hide-small"><i class="fa fa-photo w3-margin-right"></i>Photos</button>
+            <button class="w3-button w3-white w3-hide-small"><i class="fa fa-map-pin w3-margin-right"></i>Art</button>
+          </div>
+        </div>
+      </header>
+
       <h1>Filter List</h1>
-      <label htmlFor="booksPerPage">Books Per Page </label><br/>
+      <label htmlFor="booksPerPage">Books Per Page </label><br />
       <select id="booksPerPage" name="booksPerPage">
-          <option value="10">10</option>
-          <option value="10">20</option>
-          <option value="30">30</option>
-      </select><br/>
-      <label htmlFor="genre">Genre</label><br/>
+        <option value="10">10</option>
+        <option value="10">20</option>
+        <option value="30">30</option>
+      </select><br />
+      <label htmlFor="genre">Genre</label><br />
       <select id="genre" name="genre">
-          <option value="">N/A</option>
-          <option value="Action">Action</option>
-          <option value="Adventure">Adventure</option>
-          <option value="Comedy">Comedy</option>
-          <option value="Crime">Crime</option>
-          <option value="Mystery">Mystery</option>
-          <option value="Fantasy">Fantasy</option>
-          <option value="Historical">Historical</option>
-          <option value="Horror">Horror</option>
-          <option value="Romance">Romance</option>
-          <option value="Satire">Satire</option>
-          <option value="Science Fiction">Science Fiction</option>
-          <option value="Speculative">Speculative</option>
-          <option value="Thriller">Thriller</option>
-          <option value="Western">Western</option>
-      </select><br/>
-      <label htmlFor="language">Language </label><br/>
+        <option value="">N/A</option>
+        <option value="Action">Action</option>
+        <option value="Adventure">Adventure</option>
+        <option value="Comedy">Comedy</option>
+        <option value="Crime">Crime</option>
+        <option value="Mystery">Mystery</option>
+        <option value="Fantasy">Fantasy</option>
+        <option value="Historical">Historical</option>
+        <option value="Horror">Horror</option>
+        <option value="Romance">Romance</option>
+        <option value="Satire">Satire</option>
+        <option value="Science Fiction">Science Fiction</option>
+        <option value="Speculative">Speculative</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Western">Western</option>
+      </select><br />
+      <label htmlFor="language">Language </label><br />
       <select id="language" name="language">
-          <option value="">N/A</option>
-          <option value="English">English</option>
-          <option value="Mandarin Chinese">Mandarin Chinese</option>
-          <option value="Hindi">Hindi</option>
-          <option value="Spanish">Spanish</option>
-          <option value="French">French</option>
-          <option value="Arabic">Arabic</option>
-          <option value="Bengali">Bengali</option>
-          <option value="Russian">Russian</option>
-          <option value="Portugese">Portugese</option>
-          <option value="Urdu">Urdu</option>
-          <option value="Indonesian">Indonesian</option>
-          <option value="German">German</option>
-          <option value="Japanese">Japanese</option>
-          <option value="Nigerian Pidgin">Nigerian Pidgin</option>
-          <option value="Marathi">Marathi</option>
-          <option value="Telugu">Telugu</option>
-          <option value="Turkish">Turkish</option>
-          <option value="Tamil">Tamil</option>
-          <option value="Vietnamese">Vietnamese</option>
-          <option value="Tagalog">Tagalog</option>
-          <option value="Korean">Korean</option>
-          <option value="Farsi">Farsi</option>
-          <option value="Italian">Italian</option>
-          <option value="Thai">Thai</option>
-      </select><br/>
-      <label htmlFor="views">Views </label><br/>
+        <option value="">N/A</option>
+        <option value="English">English</option>
+        <option value="Mandarin Chinese">Mandarin Chinese</option>
+        <option value="Hindi">Hindi</option>
+        <option value="Spanish">Spanish</option>
+        <option value="French">French</option>
+        <option value="Arabic">Arabic</option>
+        <option value="Bengali">Bengali</option>
+        <option value="Russian">Russian</option>
+        <option value="Portugese">Portugese</option>
+        <option value="Urdu">Urdu</option>
+        <option value="Indonesian">Indonesian</option>
+        <option value="German">German</option>
+        <option value="Japanese">Japanese</option>
+        <option value="Nigerian Pidgin">Nigerian Pidgin</option>
+        <option value="Marathi">Marathi</option>
+        <option value="Telugu">Telugu</option>
+        <option value="Turkish">Turkish</option>
+        <option value="Tamil">Tamil</option>
+        <option value="Vietnamese">Vietnamese</option>
+        <option value="Tagalog">Tagalog</option>
+        <option value="Korean">Korean</option>
+        <option value="Farsi">Farsi</option>
+        <option value="Italian">Italian</option>
+        <option value="Thai">Thai</option>
+      </select><br />
+      <label htmlFor="views">Views </label><br />
       <select id="views" name="views">
-          <option value="">N/A</option>
-          <option value="desc">Descending</option>
-          <option value="asc">Ascending</option>
-      </select><br/>
-      <label htmlFor="wordcount">Word Count </label><br/>
+        <option value="">N/A</option>
+        <option value="desc">Descending</option>
+        <option value="asc">Ascending</option>
+      </select><br />
+      <label htmlFor="wordcount">Word Count </label><br />
       <select id="wordcount" name="wordcount">
-          <option value="">N/A</option>
-          <option value="desc">Descending</option>
-          <option value="asc">Ascending</option>
-      </select><br/>
-      <br/><button onClick={(e) => submitQuery(e)}>Filter</button><br/><br/>
+        <option value="">N/A</option>
+        <option value="desc">Descending</option>
+        <option value="asc">Ascending</option>
+      </select><br />
+      <br /><button onClick={(e) => submitQuery(e)}>Filter</button><br /><br />
+
+      
+      {/* Display Grid */}
+      <div class="w3-row-padding">
+        <div class="w3-third w3-container w3-margin-bottom">
+          <div class="w3-container w3-white">
+            <p><b>Lorem Ipsum</b></p>
+            <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+          </div>
+        </div>
+        <div class="w3-third w3-container w3-margin-bottom">
+          <div class="w3-container w3-white">
+            <p><b>Lorem Ipsum</b></p>
+            <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+          </div>
+        </div>
+        <div class="w3-third w3-container">
+          <div class="w3-container w3-white">
+            <p><b>Lorem Ipsum</b></p>
+            <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+          </div>
+        </div>
+      </div>
 
       {listBooksOnPage.map((book, index) => {
         return (<div key={index}>
           <hr />
-          <h1>Title: <a href={"/book/" + book._id + "/chapter/0"}>{book.title}</a></h1>
-          <p>Summary: {book.summary}</p>
-          <p>Genre: {book.genre}</p>
-          <p>Trigger Warnings: {book.tws.join(", ")}</p>
-          <p>Fandoms: {book.fandoms.join(", ")}</p>
-          <p>Characters: {book.characters.join(", ")}</p>
-          <p>Tags: {book.tags.join(", ")}</p>
-          <p>Language: {book.language}</p>
-          <p>Wordcount: {book.wordcount}</p>
-          <p>Views: {book.views}</p>
-          <p>Chapters: {book.characters.length}</p>
+          <h1>Title: <a class="bookLink" href={"/book/" + book._id + "/chapter/0"}>{book.title}</a></h1>
+          <p><b>Summary:</b> {book.summary}</p>
+          <p><b>Genre:</b> {book.genre}</p>
+          <p><b>Trigger Warnings:</b> {book.tws.join(", ")}</p>
+          <p><b>Fandoms:</b> {book.fandoms.join(", ")}</p>
+          <p><b>Characters:</b> {book.characters.join(", ")}</p>
+          <p><b>Tags:</b> {book.tags.join(", ")}</p>
+          <p><b>Language:</b> {book.language}</p>
+          <p><b>Wordcount:</b> {book.wordcount}</p>
+          <p><b>Views:</b> {book.views}</p>
+          <p><b>Chapters:</b> {book.characters.length}</p>
           <hr />
         </div>);
       })}
